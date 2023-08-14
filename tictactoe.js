d82146de1b242f4ca8a9a969fad9dd902ff42e3d
sumeetswain <sumeetswain.work@gmail.com>
2023-08-14 19:08:00 +0530

    First Commit

---
+++ tictactoe.js
@@ -0,0 +1,22 @@
+let i = 1
+let j = 9
+function xo() {
+    
+    if(i<=j){
+
+        if (i % 2 == 0 & i <= 9) {
+            console.log("O")
+            this.value = "O"
+            i++
+        }
+        else {
+            console.log("X")
+            this.value = "X"
+            i++
+        }
+    }
+
+} 
+function winner(){
+
+}
